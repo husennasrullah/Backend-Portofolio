fcdbff6bb177a5bb65e7f14fd7670db269d04dc5
Husen Nasrullah <husen.nasrullah@erajaya.com>
2024-08-19 11:33:15 +0700

    add project 3 section: grpc-auth service

---
+++ projects/project 3 - Microservices Using GRPC/go-grpc-auth/pkg/utils/jwt.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+	"github.com/husennasrullah/Backend-Portofolio/project-3/go-grpc-auth/pkg/models"
+	"time"
+)
+
+type JwtWrapper struct {
+	SecretKey       string
+	Issuer          string
+	ExpirationHours int64
+}
+
+type JwtClaims struct {
+	jwt.StandardClaims
+	Id    int64
+	Email string
+}
+
+func (w *JwtWrapper) GenerateToken(user models.User) (signedToken string, err error) {
+	claims := &JwtClaims{
+		Id:    user.Id,
+		Email: user.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(w.ExpirationHours)).Unix(),
+			Issuer:    w.Issuer,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err = token.SignedString([]byte(w.SecretKey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func (w *JwtWrapper) ValidateToken(signedToken string) (claims *JwtClaims, err error) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&JwtClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(w.SecretKey), nil
+		},
+	)
+
+	if err != nil {
+		return
+	}
+
+	claims, ok := token.Claims.(*JwtClaims)
+
+	if !ok {
+		return nil, errors.New("couldn't parse claims")
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		return nil, errors.New("JWT is expired")
+	}
+
+	return claims, nil
+
+}
